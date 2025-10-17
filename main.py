523682dad5fe0629e05ac80f745ceb0e6ffd7af8
AliZreik05 <azreik005@gmail.com>
2025-10-17 12:16:06 +0300

    added routers and fixed minor errors

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from Routes import FlashCardGenerator, QuizGenerator, LectureTranscriber, NotesS
 app = FastAPI()
 
 app.include_router(Home.router, prefix="/Home", tags=["Home"])
-app.include_router(SignUp.router, prefix="SignUp", tags=["SignUp"])
-app.include_router(SignIn.router, prefix="SignIn", tags=["SignIn"])
+app.include_router(SignUp.router, prefix="/SignUp", tags=["SignUp"])
+app.include_router(SignIn.router, prefix="/SignIn", tags=["SignIn"])
 app.include_router(QuizGenerator.router, prefix="/QuizGenerator", tags=["QuizGenerator"])
 app.include_router(FlashCardGenerator.router, prefix="/FlashCardGenerator", tags=["FlashCardGenerator"])
 app.include_router(LectureTranscriber.router, prefix="/LectureTranscriber", tags=["LectureTranscriber"])
