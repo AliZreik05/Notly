c6836855514d4c150c37666b5bf43717ff28830b
AliZreik05 <azreik005@gmail.com>
2025-10-17 11:52:36 +0300

    updated the file system and created separate routes for each feature so that each member can expand their work in the respective file

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 from fastapi import FastAPI;
+from Routes import FlashCardGenerator, QuizGenerator, LectureTranscriber, NotesSummarizer, SignIn, SignUp, Home
 
 app = FastAPI()
 
-@app.get("/home")
-def root():
-    return {"hello" : "world"}
-
-@app.get("/signup")
-def signup():
-    return 
-
-@app.get("/signin")
-def signin():
-    return
-
-app.get("/quizGenerator")
-def quizGenerator():
-    return
-
-app.get("/flashCardGenerator")
-def flashCardGenerator():
-    return
-
-app.get("/lectureTranscriber")
-def lectureTranscriber():
-    return
-
-app.get("/notesOrganizer")
-def notesOrganizer():
-    return
+app.include_router(Home.router, prefix="/Home", tags=["Home"])
+app.include_router(SignUp.router, prefix="SignUp", tags=["SignUp"])
+app.include_router(SignIn.router, prefix="SignIn", tags=["SignIn"])
+app.include_router(QuizGenerator.router, prefix="/QuizGenerator", tags=["QuizGenerator"])
+app.include_router(FlashCardGenerator.router, prefix="/FlashCardGenerator", tags=["FlashCardGenerator"])
+app.include_router(LectureTranscriber.router, prefix="/LectureTranscriber", tags=["LectureTranscriber"])
+app.include_router(NotesSummarizer.router, prefix="/NotesSummarizer", tags=["NotesSummarizer"])
