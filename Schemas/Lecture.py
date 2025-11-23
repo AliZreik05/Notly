b0f63ae2cdf993cdb6504d342b785e115b568b01
AliZreik05 <azreik005@gmail.com>
2025-11-24 00:26:39 +0200

    added lecture content to request

--- Schemas/Lecture.py
+++ Schemas/Lecture.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class LectureOut(BaseModel):
     id: int
     title: str
+    content: str
 
     class Config:
         from_attributes = True   # orm_mode=True if you use old pydantic
